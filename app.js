0ed9196bb36284b99eb6d2411428d3b53d14eb6e
wakidur <wakidur@gmail.com>
2020-04-04 07:04:56 +0600

    Everything is in one file, code does not looks maintainable and it's very hard to extend Using async function, bet there's no await Done There is no documentation ( how to run ) (Done) Rounding part is wrong (as example 0.023 should return 0.03, toFixed returned 0.02)  (Done )  RoundUpd (Done) No api calls (should not be hardcoded config) Done There should be no unused constants like http (Remove Unused http) done  for variable names we use camelCase (cashoutjuridical => cashOutJuridical) and check misspelled names (totalAmaountPerWeek) Done

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // The fs module provides an API for interacting with the file system
 const fs = require('fs');
-const http = require('http');
+const service = require('./service/service')
+
 
 /**
  * Input data ----------------------
@@ -8,178 +9,92 @@ const http = require('http');
  * Performed operations are given in that file
  */
 const input = JSON.parse(fs.readFileSync(`${__dirname}/_data/input.json`, 'utf-8'));
+const DaysOfWeek = {
+    sun: 0,
+    mon: 1,
+    tue: 2,
+    wen: 3,
+    thu: 4,
+    fri: 5,
+    sat: 6,
+}
+
+
 // Function Invock 
 calculatedCommissionFees(input);
 
 // As a single argument program must accept a path to the input file
 async function calculatedCommissionFees(inputFile) {
     try {
-        weeklyUserOperationGroup = [];
-        const configCashIn = {
-            "percents": 0.03,
-            "max": {
-                "amount": 5,
-                "currency": "EUR"
-            }
-        }
-        const cashOutNatural = {
-            "percents": 0.3,
-            "week_limit": {
-                "amount": 1000,
-                "currency": "EUR"
-            }
-        }
-        cashOutJuridical = {
-            "percents": 0.3,
-            "min": {
-                "amount": 0.5,
-                "currency": "EUR"
-            }
-        }
+      // Weekly Cash out Natural Persons Group array init 
+        let weeklyCashOutNaturalPersonsGroup = [];
+        // Http request for Config Cash In
+        let configCashIn = await service.getConfigurationEndpoint('http://private-38e18c-uzduotis.apiary-mock.com/config/cash-in');
+        // String to Object 
+        configCashIn = JSON.parse(configCashIn);
+        // Http request for Cash Out Natural Persons
+        let cashOutNatural = await service.getConfigurationEndpoint('http://private-38e18c-uzduotis.apiary-mock.com/config/cash-out/natural');
+        // String to Object
+        cashOutNatural = JSON.parse(cashOutNatural);
+        // Http request for Cash Out Legal persons
+        let cashOutJuridical = await service.getConfigurationEndpoint('http://private-38e18c-uzduotis.apiary-mock.com/config/cash-out/juridical');
+        // String to Object
+        cashOutJuridical = JSON.parse(cashOutJuridical);
         /**
          * For Cash Out ----------- Natural Persons
          * 
          */
         // Sort objects by date ascending order
         const paymentRequestSorted = inputFile.sort((a, b) => new Date(a.date) - new Date(b.date));
-        // Filter by user_type, type  and sort by user_id. 
+
+        // GET Cash In Persons Filter type. 
+        const cashInPersonsList = paymentRequestSorted.filter(item => item.type === "cash_in");
+        // Cash in Persons commission fee
+        if (cashInPersonsList && cashInPersonsList.length > 0) {
+            cashInPersonsList.forEach(element => {
+                console.log(service.particularCashInCommition(element, configCashIn));
+                return service.particularCashInCommition(element, configCashIn);
+            });
+
+        }
+        //GET Cash Out Legal persons Filter by user_type, type. 
+        const cashOutLegalPersonsList = paymentRequestSorted.filter(item => item.user_type === "juridical" && item.type === "cash_out");
+        // Cash Out Legal persons commission fee
+        if (cashOutLegalPersonsList && cashOutLegalPersonsList.length > 0) {
+            cashOutLegalPersonsList.forEach(element => {
+                console.log(service.particularCashOutJuridicalPersons(element, cashOutJuridical));
+                return service.particularCashOutJuridicalPersons(element, cashOutJuridical);
+            });
+        }
+        // User ID Group for per user operation check 
+        // GET Cash Out Natural Persons Filter by user_type, type. 
         const cashOutNaturalPersonsList = paymentRequestSorted.filter(item => item.user_type === "natural" && item.type === "cash_out");
+        // Group By UserID
         let groupByUserId = cashOutNaturalPersonsList.reduce((r, a) => {
             r[a.user_id] = [...r[a.user_id] || [], a];
             return r;
         }, {});
 
-
+        // Particular user Weely Operation Group
         for (const key in groupByUserId) {
             if (groupByUserId.hasOwnProperty(key)) {
                 const particularUserOperation = groupByUserId[key];
-                // Get User operation weekly group data 
-                weeklyUserOperationGroup.push(Object.values(particularUserOperation.reduce((acc, val) => {
-                    let dateparts = val.date.split(/T+|-|:/g);
-                    let date = new Date(dateparts[0], dateparts[1] - 1, dateparts[2]);
-                    let weekNo = getISO8601WeekNo(date);
-                    if (!acc[weekNo]) acc[weekNo] = [];
-                    acc[weekNo].push(val);
-                    return acc;
-                }, {})));
+                weeklyCashOutNaturalPersonsGroup.push(service.weekWiseGrouping(particularUserOperation, DaysOfWeek.mon, DaysOfWeek.sun));                
             }
         }
 
-        // Loop into group by id
-        weeklyUserOperationGroup.forEach(item => {
+       // Retrieve user weekly  commission fees
+        weeklyCashOutNaturalPersonsGroup.forEach(item => {
             item.forEach(nested => {
-                console.log(perticularCashOutNaturalPersons(nested, cashOutNatural));
-                return perticularCashOutNaturalPersons(nested, cashOutNatural);
+                console.log(service.particularCashOutNaturalPersons(nested, cashOutNatural));
+                // return particularCashOutNaturalPersons(nested, cashOutNatural);
             });
         })
-
-        /**
-         * For Cash In 
-         * For Cash Out ----------- Legal persons
-         * 
-         * 
-         */
-        paymentRequestSorted.forEach(item => {
-            if (item.type === "cash_in") {
-                console.log(perticularCashInCommition(item, configCashIn));
-                return perticularCashInCommition(item, configCashIn);
-            } else if (item.type === "cash_out" && item.user_type === "juridical") {
-                console.log(perticularCashOutJuridicalPersons(item, cashOutJuridical));
-                return perticularCashOutJuridicalPersons(item, cashOutJuridical);
-            }
-        });
 
     } catch (error) {
         console.error(error);
     }
 }
 
-function perticularCashInCommition(item, configcashin) {
-    let commission = 0;
 
-    if (item.operation && item.operation.amount && item.operation.currency === "EUR" && item.operation.currency === configcashin.max.currency) {
-        let amount = parseFloat(item.operation.amount);
-        let percents = parseFloat(configcashin.percents)
-        commission += (amount / 100) * percents;
-
-        if (parseFloat(commission) > parseFloat(configcashin.max.amount)) {
-            commission = parseFloat(configcashin.max.amount).toFixed(2);
-        } else {
-            commission.toFixed(2);
-        }
-
-    } else {
-        console.error(" Some this Wrong ");
-
-    }
-    return commission;
-}
-
-function perticularCashOutJuridicalPersons(item, cashoutjuridical) {
-    let commission = 0;
-    if (item.operation && item.operation.amount && item.operation.currency === "EUR" && item.operation.currency === cashoutjuridical.min.currency) {
-        let amount = parseFloat(item.operation.amount);
-        let percents = parseFloat(cashoutjuridical.percents)
-        // Not less than 0.50 EUR for operation
-        if (parseFloat(item.operation.amount) > parseFloat(cashoutjuridical.min.amount)) {
-            commission = parseFloat((amount / 100) * percents).toFixed(2);
-        } else {
-            console.error("Less than 0.50 EUR");
-        }
-    } else {
-        console.error(" Some this Wrong ");
-
-    }
-    return commission;
-
-}
-
-function perticularCashOutNaturalPersons(weeklyOperation, cashoutnatural) {
-    let commission = 0;
-    totalAmaountPerWeek = 0;
-    if (weeklyOperation.length > 0) {
-        weeklyOperation.forEach(item => {
-            if (item.operation && item.operation.currency === "EUR") {
-                totalAmaountPerWeek += item.operation.amount;
-            }
-        })
-    }
-    if (totalAmaountPerWeek > cashoutnatural.week_limit.amount) {
-        let perWeek = parseFloat(totalAmaountPerWeek) - parseFloat(cashoutnatural.week_limit.amount);
-        let amount = parseFloat(perWeek);
-        let percents = parseFloat(cashoutnatural.percents)
-        commission = parseFloat((amount / 100) * percents).toFixed(2);
-    } else {
-        // Default commission fee - 0.3% from cash out amount.
-        let amount = parseFloat(totalAmaountPerWeek);
-        let percents = parseFloat(cashoutnatural.percents)
-        commission = parseFloat((amount / 100) * percents).toFixed(2);;
-    }
-
-    return commission;
-
-}
-
-function getISO8601WeekNo(date) {
-    let startDate = startOfWeek(new Date(date.getFullYear(), 0));
-    let endDate = endOfWeek(new Date(date.getFullYear(), date.getMonth(), date.getDate()));
-    while (endDate.getDay() < 6) endDate.setDate(endDate.getDate() + 1);
-    endDate = endDate.getTime();
-    let weekNo = 0;
-    while (startDate.getTime() < endDate) {
-        if (startDate.getDay() == 4) weekNo++;
-        startDate.setDate(startDate.getDate() + 1);
-    }
-    return weekNo;
-}
-// Get the week start date
-function startOfWeek(date) {
-    let diff = date.getDate() - date.getDay() + (date.getDay() === 0 ? -6 : 1);
-    return new Date(date.setDate(diff));
-}
-// Get the week end date
-function endOfWeek(date) {
-    let lastday = date.getDate() - (date.getDay() - 1) + 6;
-    return new Date(date.setDate(lastday));
-}
 
---
+++ service/service.js
@@ -0,0 +1,197 @@
+// request is defined as a peer-dependency and thus has to be installed separately
+const request = require('request');
+const rp = require('request-promise');
+
+module.exports = {
+    getConfigurationEndpoint,
+    particularCashInCommition,
+    particularCashOutJuridicalPersons,
+    weekWiseGrouping, 
+    particularCashOutNaturalPersons
+
+
+};
+
+const DaysOfWeek = {
+    sun: 0,
+    mon: 1,
+    tue: 2,
+    wen: 3,
+    thu: 4,
+    fri: 5,
+    sat: 6,
+}
+
+
+function getConfigurationEndpoint(path) {
+    return rp(path)
+        .then((data) => {
+            return data;
+        })
+        .catch((err) => {
+            return err;
+        });
+}
+
+function particularCashInCommition(item, configCashIn) {
+    let commission = 0;
+
+    if (item.operation && item.operation.amount && item.operation.currency === "EUR" && item.operation.currency === configCashIn.max.currency) {
+        let amount = parseFloat(item.operation.amount);
+        let percents = parseFloat(configCashIn.percents)
+        commission += (amount / 100) * percents;
+
+        if (parseFloat(commission) > parseFloat(configCashIn.max.amount)) {
+            //  example 0.023 should return 0.03
+            commission = roundUp(configCashIn.max.amount, 2).toFixed(2);
+        } else {
+            commission = roundUp(commission, 2).toFixed(2);
+        }
+
+    } else {
+        console.error(" Some this Wrong ");
+
+    }
+    return commission;
+}
+
+// Cash Out Natural Persons commintion fee calculating 
+function particularCashOutJuridicalPersons(item, cashOutJuridical) {
+    let commission = 0;
+    if (item.operation && item.operation.amount && item.operation.currency === "EUR" && item.operation.currency === cashOutJuridical.min.currency) {
+        let amount = parseFloat(item.operation.amount);
+        let percents = parseFloat(cashOutJuridical.percents)
+        // Not less than 0.50 EUR for operation
+        if (parseFloat(item.operation.amount) > parseFloat(cashOutJuridical.min.amount)) {
+            //  example 0.023 should return 0.03
+            commission = roundUp(((amount / 100) * percents), 2).toFixed(2);     
+        } else {
+            console.error("Less than 0.50 EUR");
+        }
+    } else {
+        console.error(" Some this Wrong ");
+
+    }
+    return commission;
+
+}
+
+
+
+// Cash Out Natural Persons commintion fee calculating 
+function particularCashOutNaturalPersons(weeklyOperation, cashoutnatural) {
+    let commission = 0.00;
+    totalAmountPerWeek = 0;
+    if (weeklyOperation.transactions.length > 0) {
+        weeklyOperation.transactions.forEach(item => {
+            if (item.operation && item.operation.currency === "EUR") {
+                totalAmountPerWeek += item.operation.amount;
+            }
+        })
+    }
+    if (totalAmountPerWeek > cashoutnatural.week_limit.amount) {
+        commission = 0;
+        // If total cash out amount is exceeded - commission is calculated only from exceeded amount
+        let perWeek = parseFloat(totalAmountPerWeek) - parseFloat(cashoutnatural.week_limit.amount);
+        let amount = parseFloat(perWeek);
+        let percents = parseFloat(cashoutnatural.percents)
+        commission = roundUp(((amount / 100) * percents), 2).toFixed(2);
+    } else if(parseInt(totalAmountPerWeek)  <= parseInt(cashoutnatural.week_limit.amount)) {
+        // for 1000.00 EUR there is still no commission fee
+        commission = "0.00" ;
+    }
+
+    return commission;
+
+}
+
+// Will return a array of object.
+// Each object will have startDateOfWeek, endDateOfWeek, transactions
+function weekWiseGrouping(transactions, startDayOfWeek, endDayOfWeek) {
+    // sort the transations
+    transactions.sort(function (transaction) {
+        return transaction.date > transaction.date ? -1 : 1
+    })
+
+    let index = 0
+    let weekWiseGroup = []
+    while (index < transactions.length) {
+        let transaction = transactions[index]
+        const startDateOfWeek = getPreceedingDayDate(
+            new Date(transaction.date),
+            startDayOfWeek
+        )
+        const endDateOfWeek = getNextDayDate(
+            new Date(transaction.date),
+            endDayOfWeek
+        )
+
+        const transactionsWithinSameWeek = [transaction]
+        index++
+        while (
+            index < transactions.length &&
+            startDateOfWeek <= new Date(transactions[index].date) && // Optimization possible createing single Date() object
+            new Date(transactions[index].date) <= endDateOfWeek
+        ) {
+            transactionsWithinSameWeek.push(transactions[index])
+            index++
+        }
+
+        weekWiseGroup.push({
+            startDateOfWeek: startDateOfWeek,
+            endDateOfWeek: endDateOfWeek,
+            transactions: transactionsWithinSameWeek,
+        })
+    }
+
+    return weekWiseGroup
+}
+
+
+
+// First parameter is number that you want to round. Second parameter is number (integer) of numbers after point that you want to get
+function roundUp(num, precision) {
+    precision = Math.pow(10, precision)
+    return Math.ceil(num * precision) / precision
+}
+
+
+
+function getPreceedingDayDate(date, dayOfWeek) {
+    const day = date.getDay()
+
+    let dayToMinus = 0
+    if (dayOfWeek < day) {
+        dayToMinus = day - dayOfWeek
+    } else if (dayOfWeek > day) {
+        dayToMinus = day + 1 + (DaysOfWeek.sat - dayOfWeek)
+    }
+
+    const preceededDate = new Date(date)
+    preceededDate.setDate(preceededDate.getDate() - dayToMinus)
+    return preceededDate
+}
+
+function getNextDayDate(date, dayOfWeek) {
+    const day = date.getDay()
+
+    let dayToAdd = 0
+
+    if (day < dayOfWeek) {
+        dayToAdd = dayOfWeek - day
+    } else if (day > dayOfWeek) {
+        dayToAdd = dayOfWeek + 1 + (DaysOfWeek.sat - day)
+    }
+
+    const nextDate = new Date(date)
+    nextDate.setDate(nextDate.getDate() + dayToAdd)
+    return nextDate
+}
+
+
+
+// First parameter is number that you want to round. Second parameter is number (integer) of numbers after point that you want to get
+function roundUp(num, precision) {
+    precision = Math.pow(10, precision)
+    return Math.ceil(num * precision) / precision
+}
